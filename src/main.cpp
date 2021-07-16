7f2ffb83c682e2ab283d25b04a3cdf6d4066e101
adel <adill.u@outlook.com>
2021-07-16 15:36:01 +0200

    GUI completed, bugs fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,130 @@
 #include <SFML/Graphics.hpp>
-sf::Texture t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12;
-const int bKING = 6;
-const int bQUEEN = 5;
-const int bROCK = 4;
-const int bBISHOP = 3;
-const int bKNIGHT = 2;
-const int bPAWN = 1;
+#include <iostream>
+#include <math.h>
 
-const int wKING = -6;
-const int wQUEEN = -5;
-const int wROCK = -4;
-const int wBISHOP = -3;
-const int wKNIGHT = -2;
-const int wPAWN = -1;
+// BOARD 
 
-const int FREESPOT = 0;
+
+const int8_t bKING = 1;
+const int8_t bQUEEN = 2;
+const int8_t bROCK = 3;
+const int8_t bBISHOP = 4;
+const int8_t bKNIGHT = 5;
+const int8_t bPAWN = 6;
+const int8_t wKING = -1;
+const int8_t wQUEEN = -2;
+const int8_t wROCK = -3;
+const int8_t wBISHOP = -4;
+const int8_t wKNIGHT = -5;
+const int8_t wPAWN = -6;
+const int8_t emptySquare = 0;
+
+int8_t currentFigure = 0;
+
+int8_t board[8][8] = {
+{wROCK,wKNIGHT,wBISHOP,wQUEEN,wKING,wBISHOP,wKING,wROCK},
+{wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN},
+{emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare},
+{emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare},
+{emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare},
+{emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare,emptySquare},
+{bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN},
+{bROCK,bKNIGHT,bBISHOP,bQUEEN,bKING,bBISHOP,bKNIGHT,bROCK},
+};
+
+
+// Figures
+
+sf::Texture bk, bq, br, bb, bn, bp, wk, wq, wr, wb, wn, wp;
+sf::Sprite Figures[32];
+
+int cnt = 0;
+void loadPositions(){
+    for (size_t i = 0; i < 8; i++)
+    {
+        for (size_t j = 0; j < 8; j++)
+        {
+            int n = board[i][j];
+            if(n==0){continue;}
+            if (n == bKING)
+            {
+                Figures[cnt].setTexture(bk);
+            }
+            else if (n == bQUEEN)
+            {
+                Figures[cnt].setTexture(bq);
+            }
+            else if (n == bROCK)
+            {
+                Figures[cnt].setTexture(br);
+            }
+            else if (n == bBISHOP)
+            {
+                Figures[cnt].setTexture(bb);
+            }
+            else if (n == bKNIGHT)
+            {
+                Figures[cnt].setTexture(bn);
+            }
+            else if (n == bPAWN)
+            {
+                Figures[cnt].setTexture(bp);
+            }
+            else if (n == wKING)
+            {
+                Figures[cnt].setTexture(wk);
+            }
+            else if (n == wQUEEN)
+            {
+                Figures[cnt].setTexture(wq);
+            }
+            else if (n == wROCK)
+            {
+                Figures[cnt].setTexture(wr);
+            }
+            else if (n == wBISHOP)
+            {
+                Figures[cnt].setTexture(wb);
+            }
+            else if (n == wKNIGHT)
+            {
+                Figures[cnt].setTexture(wn);
+            }
+            else if (n == wPAWN)
+            {
+                Figures[cnt].setTexture(wp);
+            }
+            Figures[cnt].setPosition(100*j,100*i);
+            cnt++;
+        }
+        
+    }
+    
+}
+
+struct posistion 
+{
+   int x;
+   int y; 
+};
+struct Square
+{
+    int8_t x;
+    int8_t y;
+};
+
+posistion oldPos, newPos;
+Square currentSquare, newSquare;
+int currentPiece;
+
 
 int squareSize = 100;
 sf::Color col1 = sf::Color(232,235,239);
 sf::Color col2 = sf::Color(125,135,150);
 sf::Vector2f sc(0.05,0.05);
+sf::Vector2i pos;
 
-int board[8][8] = {
-{wROCK,wKNIGHT,wBISHOP,wQUEEN,wKING,wBISHOP,wKING,wROCK},
-{wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN},
-{FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT},
-{FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT},
-{FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT},
-{FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT,FREESPOT},
-{bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN},
-{bROCK,bKNIGHT,bBISHOP,bQUEEN,bKING,bBISHOP,bKNIGHT,bROCK},
-};
+
 
 void drawBoard(sf::RenderWindow &window, sf::RectangleShape rec){
 for (size_t j = 0; j < 8; j++)
@@ -44,136 +138,111 @@ for (size_t j = 0; j < 8; j++)
         }
 }
 
+
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 800), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Chess!");
+    sf::RectangleShape square;
+    square.setSize(sf::Vector2f(squareSize, squareSize));
+    sf::Texture texture;
+    bool isMoving = false;
+    float dx = 0, dy=0;
+    texture.loadFromFile("assets/bK.png");
+    sf::Sprite piece;
+    piece.setTexture(texture);
+    float xpos = 0, ypos = 0;
+    
+    //// 
 
-    // run the program as long as the window is open
+    bk.loadFromFile("assets/bK.png");
+    bq.loadFromFile("assets/bQ.png");
+    br.loadFromFile("assets/bR.png");
+    bb.loadFromFile("assets/bB.png");
+    bn.loadFromFile("assets/bN.png");
+    bp.loadFromFile("assets/bP.png"); 
+    wk.loadFromFile("assets/wK.png");
+    wq.loadFromFile("assets/wQ.png");
+    wr.loadFromFile("assets/wR.png");
+    wb.loadFromFile("assets/wB.png");    
+    wn.loadFromFile("assets/wN.png");
+    wp.loadFromFile("assets/wP.png");
+
+    loadPositions();
+
+   
+    
+
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::RectangleShape rec;
-        rec.setSize(sf::Vector2f(squareSize, squareSize));
-        sf::Event event;
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window); // Get's updated all the time => while 
         
-        t1.loadFromFile("assets/bK.png");
-        t2.loadFromFile("assets/bQ.png");
-        t3.loadFromFile("assets/bR.png");
-        t4.loadFromFile("assets/bB.png");
-        t5.loadFromFile("assets/bN.png");
-        t6.loadFromFile("assets/bP.png"); 
-        t7.loadFromFile("assets/wK.png");
-        t8.loadFromFile("assets/wQ.png");
-        t9.loadFromFile("assets/wR.png");
-        t10.loadFromFile("assets/wB.png");
-        t11.loadFromFile("assets/wN.png");
-        t12.loadFromFile("assets/wP.png");
-
-        sf::Sprite bk(t1), bq(t2), br(t3), bb(t4), bn(t5), bp(t6), wk(t7), wq(t8), wr(t9), wb(t10), wn(t11), wp(t12);
- 
+        
+        
+        sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed){
-                window.close();}
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
             else if (event.type == sf::Event::MouseButtonPressed)
-                {
+            {
+                // Which Figure is being pressed? 
+                   for (size_t i = 0; i < 32; i++)
+                   {
+                       if(Figures[i].getGlobalBounds().contains(mousePos.x,mousePos.y)){
+                           isMoving = true; // True if one of the figures is pressed 
+
+                           currentSquare.x = round(sf::Mouse::getPosition(window).x/100);
+                           currentSquare.y = round(sf::Mouse::getPosition(window).y/100);
+
+                           oldPos.x = currentSquare.x*100; oldPos.y = currentSquare.y*100;
+
+                           currentPiece = board[currentSquare.y][currentSquare.x];
+                           currentFigure = i;
+                           xpos = 100*currentSquare.x;
+                           ypos = 100*currentSquare.y;
+                           dx = mousePos.x - xpos;
+                           dy = mousePos.y - ypos;
+                       }
+                   }
+
+
+            }
+            if(event.type==sf::Event::MouseButtonReleased){
+                isMoving = false;
+                newSquare.x = round(Figures[currentFigure].getPosition().x/100); 
+                newSquare.y = round(Figures[currentFigure].getPosition().y/100);
+                newPos.x = 100 * newSquare.x;
+                newPos.y = 100 * newSquare.y;
+                if(currentPiece == wPAWN || currentPiece==bPAWN){
+                    if (abs(currentSquare.y - newSquare.y)<3)
+                    {
+                        Figures[currentFigure].setPosition(newPos.x,newPos.y);
+                    }
+                    else{Figures[currentFigure].setPosition(oldPos.x,oldPos.y);}
                     
+                }else{Figures[currentFigure].setPosition(oldPos.x,oldPos.y);}
+                
+
+                //std::cout << "Old Sqaure:" <<currentSquare.x << "," <<currentSquare.y << "\n" << "newSq" << newSquare.x << "," << newSquare.y << "\n";
+            }
+            
+            if (isMoving) {
+                Figures[currentFigure].setPosition(mousePos.x-dy,mousePos.y-dy);
                 }
-                
-              
-                
         }
 
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-        // draw everything here...
-        // window.draw(...);
-        drawBoard(window,rec);
+        window.clear();
 
-        // Updateboard
-        for (size_t i = 0; i < 8; i++)
-        {
-           for (size_t j = 0; j < 8; j++)
-           {
-               if (board[i][j]==wPAWN)
-               {
-                   wp.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(wp);
-               }
-               else if (board[i][j]==bPAWN)
-               {
-                   bp.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(bp);
-               }
-               else if (board[i][j]==bKNIGHT)
-               {
-                   bn.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(bn);
-               }
-               else if (board[i][j]==wKNIGHT)
-               {
-                   wn.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(wn);
-               }
-               else if (board[i][j]==wBISHOP)
-               {
-                   wb.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(wb);
-
-               }
-               else if (board[i][j]==bBISHOP)
-               {
-                   bb.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(bb);
-               }
-
-
-               else if (board[i][j]==bKING)
-               {
-                   bk.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(bk);
-               }
-               else if (board[i][j]==wKING)
-               {
-                   wk.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(wk);
-               }
-               else if (board[i][j]==bQUEEN)
-               {
-                   bq.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(bq);
-               }
-               else if (board[i][j]==wQUEEN)
-               {
-                   wq.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(wq);
-               }
-               else if (board[i][j]==bROCK)
-               {
-                   br.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(br);
-               }
-               else if (board[i][j]==wROCK)
-               {
-                   wr.setPosition(sf::Vector2f(j*100,i*100));
-                   window.draw(wr);
-               }
-               // END 
-
-
-
-
-               
-               
-               
-               
-           }
-           
-        }
         
-        // end the current frame
+        drawBoard(window,square);
+        //piece.setPosition(sf::Vector2f(0,0));
+        for (size_t i = 0; i < 32; i++)
+        {
+            window.draw(Figures[i]);
+        }
+
         window.display();
     }
 
